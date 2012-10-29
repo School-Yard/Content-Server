e24a466e25d28359f507d776d8398562635f091d
Cody Stoltman <particlebanana@gmail.com>
2012-10-29 16:00:16 -0500

    test access control on non-get routes

---
+++ test/access_control/buckets/destroy.js
@@ -0,0 +1,60 @@
+var helpers = require('../../support/helpers'),
+    request = require('supertest'),
+    should = require('should');
+
+describe('Access Control', function() {
+  describe('Buckets#destroy', function() {
+
+    describe('authorized', function() {
+      var app;
+
+      before(function(done) {
+        helpers.buildServer(function(connection, server) {
+          app = server;
+
+          app.before(function(req, res, next) {
+            req.user = {role: 2};
+            next();
+          });
+
+          done();
+        });
+      });
+
+      it('should require role 2', function(done) {
+        request(app)
+        .del('/api/v1/buckets/100')
+        .end(function(err, res){
+          res.status.should.not.equal(401)
+          done();
+        });
+      });
+    });
+
+    describe('unauthorized', function() {
+      var app;
+
+      before(function(done) {
+        helpers.buildServer(function(connection, server) {
+          app = server;
+
+          app.before(function(req, res, next) {
+            req.user = {role: 1};
+            next();
+          });
+
+          done();
+        });
+      });
+
+      it('should require role 2', function(done) {
+        request(app)
+        .del('/api/v1/buckets/100')
+        .end(function(err, res){
+          res.status.should.equal(401)
+          done();
+        });
+      });
+    });
+  });
+});
